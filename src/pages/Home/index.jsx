09b973c15c72f2a2e535366b634dcad87f903781
Kirill Shchipanov <shchipanovk@mail.ru>
2024-06-25 10:17:24 +0300

    adaptive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import styles from './Home.module.scss'
 const Home = () => {
   return (
       <div className={styles.root}>
-        <h1>Kosygin</h1>
+        <h1>Добро пожаловать на сайт проекта "Глазами Косыгина!" Чувствуйте себя как дома!</h1>
       </div>
   )
 }
